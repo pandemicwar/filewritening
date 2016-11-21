767ff4410eac391246cb5a8df85d9eb944f41a0e
pandemicwar <VasilevVS007@outlook.com>
2016-11-22 01:44:16 +0400

    Update fout.cpp

--- fout.cpp
+++ fout.cpp
@@ -1,14 +1,28 @@
-#include <fstream>
-#include <iostream>
 #include <string>
- 
-using namespace std;
-void main(void)
+#include <iostream> 
+#include <fstream>
+int main()
 {
-FILE *f;
-string a;
-f = fopen("1.txt","w");
-ofstream fout ("1.txt"); 
-fout << "3455646";
-fclose(f);
+    setlocale (LC_ALL, "Russian");
+    std::string bufStr, text = "";
+    std::ifstream inp("text.txt", std::ios::in);
+    inp.seekg(0);
+    if(!inp)
+    {
+        std::cout<<"Файла нет\n";
+        system("pause");
+        return 0;
+    }
+    while(!inp.eof())
+    {
+        getline(inp, bufStr, '\n');
+        text += bufStr + '\n';
+    }   
+    std::cout<<text<<'\n';
+    inp.close();
+    std::ofstream out("text.txt", std::ios::app);
+    out<<"\nЗапись завершена";
+    out.close();
+    system("pause");
+    return 0;
 }
